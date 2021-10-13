f0e07889938e14cf946d4d74b268ab26079a5d34
GamingInfinite <ender2gamer123@gmail.com>
2021-10-12 20:14:57 -0500

    got it working cuz I was stupid

--- src/main/java/com/ginfinity/rnditemxsec/RNDItemXSec.java
+++ src/main/java/com/ginfinity/rnditemxsec/RNDItemXSec.java
@@ -1,14 +1,20 @@
-package net.fabricmc.example;
+package com.ginfinity.rnditemxsec;
 
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loom.util.Constants;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import org.apache.commons.logging.Log;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
+public class RNDItemXSec implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger("rnditemxsec");
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +22,11 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("RandomItemGiver Initializing...");
+		for (int i = 0; i < ; i++) {
+			LOGGER.info(Item.BLOCK_ITEMS.get(i));
+		}
+
+
 	}
 }
